7f768c530309e9b8191f9a019f82c003f4c898f3
Mandie <amandine.rondeaux0703@gmail.com>
2020-10-01 12:09:21 +0200

    build app.js install body-parser package&configure json on express configuration for saving requests

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const express =require('express');
 const app= express();
 const port=3000;
 const mongoose=require('mongoose');
-
+const bodyParser=require ('body-parser');
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 //MONGOOSE create connection to DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true ,useUnifiedTopology: true })
+const connectionToDB=async()=>{
+try{
+await mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true ,useUnifiedTopology: true })
+}
+catch (error){
+  console.log(`Oooupps there is this error ${error}`);
+}
+}
+connectionToDB();
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -16,6 +27,7 @@ db.once('open', function() {
 //routes
 
 const authRoute=require('./routes/Auth');
+const { json } = require('body-parser');
 app.use('/api/user',authRoute);
 
 app.listen (port,()=>console.log(`server is running on port ${port}`))
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,11 @@
 const router=require('express').Router();
+const User=require('../models/User');
 
 router.post("/register",(req,res)=>{
-    res.send("register page");
+    const {name,email,password}=req.body;
+    let newUser=new User(
+        {name,email,password}
+    )
 })
 
 module.exports=router;
