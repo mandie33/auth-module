0cf7b93c13305fd5707f67f908216125a5a73d3f
Mandie <amandine.rondeaux0703@gmail.com>
2020-10-02 13:42:51 +0200

    ci/Auth.js login function/add login request/login fn added

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,8 @@
 const router=require('express').Router();
-const { findOne } = require('../models/User');
 const User=require('../models/User');
 const {registerData,loginData}=require("./validation");
 const bcrypt=require('bcrypt');
+const jwt=('require jsonwebtoken');
 
 
 router.post("/register",async(req,res)=>{
@@ -16,16 +16,30 @@ router.post("/register",async(req,res)=>{
     //hash password
     const salt= await bcrypt.genSalt(10);
     const hashedPassword= await bcrypt.hash(req.body.password, salt);
-
+    //register new user
     existingUser?res.status(400).send("this user already exists"):firstUser=new User(
         {name,email,password:hashedPassword})
     try{
-        const savedUser=await firstUser.save();
-        res.send(savedUser);
+        await firstUser.save();
+        res.send({user:firstUser._id});
     }
     catch(error){console.log(error)}
-    // .then (()=> newUser.saved())
-    // .catch(errors=>res.send(errors))
+    
+})
+
+router.post("/login",async(req,res)=>{
+        //validate data before submitting:
+    const {error}=loginData(req.body);
+    error?res.status(400).send(error.details[0].message):"";
+        //checking if a user's email is the same as this already saved:
+    let existingUser=await User.findOne({email:req.body.email});
+    !existingUser?res.send("email or pwd don't exist"):"";
+    //pwd correct?
+    let validPwd=bcrypt.compare(req.body.password,existingUser.password );
+    !validPwd?res.send("ooouupps email or pwd don't exist "): res.send("success");
+
+    //create and assign a token
+    
 })
 
 module.exports=router;
--- routes/validation.js
+++ routes/validation.js
@@ -11,7 +11,7 @@ const schema=Joi.object({
 
 const loginData=data=>{
     const schema=Joi.object({
-        name:Joi.string().min(6).required(),
+        
         email:Joi.string().min(6).required().email(),
         password:Joi.string().min(6).required(),
     })
