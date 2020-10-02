9da9c87a59b0a71ec922647b0ceb2a9f26a9ad77
Mandie <amandine.rondeaux0703@gmail.com>
2020-10-02 13:02:04 +0200

    ci/Auth.js/add email control & hash pwd/ checking if user exists & securizing pwd

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,15 +1,24 @@
 const router=require('express').Router();
+const { findOne } = require('../models/User');
 const User=require('../models/User');
 const {registerData,loginData}=require("./validation");
+const bcrypt=require('bcrypt');
 
 
 router.post("/register",async(req,res)=>{
-    const {name,email,password}=req.body;
+    const {name,email}=req.body;
     let firstUser;
     //validate data before submitting:
     const {error}=registerData(req.body);
-    error?res.status(400).send(error.details[0].message):firstUser=new User(
-        {name,email,password})
+    error?res.status(400).send(error.details[0].message):"";
+    //checking if a user already exists:
+    let existingUser=await User.findOne({email:req.body.email});
+    //hash password
+    const salt= await bcrypt.genSalt(10);
+    const hashedPassword= await bcrypt.hash(req.body.password, salt);
+
+    existingUser?res.status(400).send("this user already exists"):firstUser=new User(
+        {name,email,password:hashedPassword})
     try{
         const savedUser=await firstUser.save();
         res.send(savedUser);
